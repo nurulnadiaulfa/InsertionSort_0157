1fca7a90fafc597b300e8d5b52f62d7bea1097c1
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-03-25 15:58:29 +0700

    menambahkan insertionsort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -31,3 +31,27 @@ void input()
         cin >> arr[i]; // Menyimpan nilai data ke n kedalam array arr
     }
 }
+
+void insertionSort()
+{ // Procedure InsertionSort
+    
+    int temp; // Membuat variable data temporer atau penyimpanan sementara
+    int j, i; // Membuat variable j sebagai penanda
+
+    for (i = 1; i <= n - 1; i++)
+    { // step 1
+        
+        temp = arr[1]; // step 2
+
+        j = i - 1; // step 3
+
+        while (j>= 0 && arr[j] > temp) // step 4
+        { 
+            arr[j + 1 ] = arr[j]; // step 4a
+            j--; // step 4b
+        }
+
+        arr[j + 1] = temp; // step 5
+    }
+}
+
