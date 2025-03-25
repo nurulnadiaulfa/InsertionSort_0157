b62bdc45a59be82eff47bd93e52c47940c6d8d12
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-03-25 15:50:46 +0700

    menambahkan procedure input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,24 @@ using namespace std;
 
 int arr[20]; // membuat array dengan panjang data 20
 int n; // membuat variable inputan n
+
+void input()
+{ // procedure input
+    while (true)
+    { 
+        cout << "Masukkan Jumlah Data pada Array : "; // membuat inputan jumlah element array
+        cin >> n; // memanggil variable inputan n
+
+        if (n <= 20)
+        { // membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        { 
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+        }
+    }
+    cout << endl; // Membuat jarak per baris program
+    cout << "======================" << endl; // Membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
